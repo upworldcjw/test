d6424cfaa8618551af722064917f53d27193a513
jianwei <jianwei.chen@pengpengla.com>
2016-11-22 19:22:43 +0800

    优先级view封装

---
+++ ULPriorityView/ULPriorityTest.h
@@ -0,0 +1,15 @@
+//
+//  ULPriorityTest.h
+//  UpLive
+//
+//  Created by jianwei on 17/11/2016.
+//  Copyright © 2016 AsiaInnovations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ULPriorityTest : NSObject
+
++ (void)test;
+
+@end
