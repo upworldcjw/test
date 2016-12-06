9196c56228659d0e3b13264d0d03962399ce5fc8
jianwei <jianwei.chen@pengpengla.com>
2016-12-06 11:05:15 +0800

    fix  the question http://stackoverflow.com/questions/35931789/why-do-i-fail-to-get-textcontainers-rect-by-using-usedrectfortextcontainer

---
+++ TestTextKit/TestTextKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTextKit
+//
+//  Created by jianwei on 06/12/2016.
+//  Copyright © 2016 jianwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
